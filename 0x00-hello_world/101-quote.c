cda437ba4fa3049905adfa86312d7df9ebcdd6b3
WCMFRED <comm11jj@gmail.com>
2023-02-20 10:56:34 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - This is the main function
+ *
+ * Description - beginning of the file
+ *
+ * Return : 1 as success
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
