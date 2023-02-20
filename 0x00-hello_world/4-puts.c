5c01cefed4740b93154323b271d3236e8e95dc97
WCMFRED <comm11jj@gmail.com>
2023-02-20 10:27:58 -0800

    using puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - this is the main function
+ *
+ * description: starting of the program
+ *
+ * Return : 0 as success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like builiding a multilingual puzzle");
+	return(0);
+}
