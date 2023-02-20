f0a76de4dc2eac54bcdbf2c1f95323a7bf26dbbc
WCMFRED <comm11jj@gmail.com>
2023-02-20 11:03:05 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,11 +10,7 @@
 
 int main(void)
 {
-	if (write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58) != 58)
-	{
-	    write(2, "null\n", 6);
-	    return (-1);
-	}
-	return (0);
-
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
