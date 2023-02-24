1ff40a8f5bf85a5072e2fc8770598e034cf0d4eb
WCMFRED <comm11jj@gmail.com>
2023-02-24 00:27:30 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints num
+ *
+ * Description - null
+ *
+ * Return: 0 As success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf('\n');
+	return (0);
+}
