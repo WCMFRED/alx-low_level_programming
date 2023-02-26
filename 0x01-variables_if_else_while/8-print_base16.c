d66b71c2e5e191a2aef644bcf5ea089cc94cdc75
WCMFRED <comm11jj@gmail.com>
2023-02-26 08:05:52 -0800

    prints base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,11 +13,10 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = 0; i< 10; i++)
+	for (i = 0; i < 10; i++)
 		putchar((i % 10) + '0');
-	for (j = 'A'; j < 'G'; j++)
+	for (j = 'a'; j < 'g'; j++)
 		putchar(j);
 	putchar('\n');
 	return (0);
-}
-			
+}			
