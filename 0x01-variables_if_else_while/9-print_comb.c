a22b9cdcf69d5e15ec8a03873b66da512ff96fc6
WCMFRED <comm11jj@gmail.com>
2023-02-26 09:01:53 -0800

    prints combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
+	int num;
+	
+	for (num = 0; num <= 9; num++)
 	{
-		putchar((i %  10) + '0');
+		putchar((num % 10) + '0');
 		
-		if (i < 9)
+		for (num < 9)
 		{
 			putchar(',');
 			putchar(' ');
