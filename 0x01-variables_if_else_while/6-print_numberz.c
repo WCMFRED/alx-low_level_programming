b83a3c78d92d0f56aa2441661427bbf109dee3a1
WCMFRED <comm11jj@gmail.com>
2023-02-26 07:32:03 -0800

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ *
+ * Description - null
+ *
+ * Return : 0 success
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	return (0);
+}
