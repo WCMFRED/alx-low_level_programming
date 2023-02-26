5b61d9ecd995f57f61f6ec267eba46e3b908b4a6
WCMFRED <comm11jj@gmail.com>
2023-02-26 08:02:46 -0800

    prints base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - base 16
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i< 10; i++)
+		putchar((i % 10) + '0');
+	for (j = 'A'; j < 'G'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
+			
