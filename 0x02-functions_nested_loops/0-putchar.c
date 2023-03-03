c43a35402514c7a110f9ae42f1586f0b4021fe8c
WCMFRED <comm11jj@gmail.com>
2023-03-03 03:52:48 -0800

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - return program that prints putchar
+ *
+ * Description : prints putchar
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
